838173ac1541edeab1cb6cecd983ec989c66acaf
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-08 14:12:27 -0800

    Header placeholder written

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>AC VILLAGERS</h1>
+      </div>
+    );
+  }
+}
